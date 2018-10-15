bb51a540bda4fb5b1c716d4e0a3fb712024dcc53
Python07 <Python07@sirjd.internal>
2018-10-15 19:25:39 +0100

    My WIP code

--- intro_python/python/wk03/question01.py
+++ intro_python/python/wk03/question01.py
@@ -1,14 +1,18 @@
-# A simple Y/N validation function
-# q_text = Question text
-q_txt = 'Are we learning any python yet'
-
-def get_answer (q_text):
-    return True
-
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: '))
+        if x.lower() == "y" or x.upper() == "YES":
+            gotanswer = True
+            return True
+        elif x.lower() == "n" or x.upper() == "NO":
+            gotanswer = True
+            return False      
 
 # Ask question and define result
-if get_answer(q_txt):
+if get_answer('Are we learning any python yet'):
     print('Yes')
 else:
     print('No')
-
--- intro_python/python/wk03/question02.py
+++ intro_python/python/wk03/question02.py
@@ -4,14 +4,20 @@ data = {"Q": "Is it an aquatic animal"}
 # A simple Y/N validation function
 # q_text = Question text
 # Get question from data
-q_txt = ' '
 
-def get_answer (q_text):
-    return True
-
+def get_answer (question):
+    gotanswer = False
+    while gotanswer == False:
+        x = str(input(question + '? [Y/N]: '))
+        if x.lower() == "y" or x.upper() == "YES":
+            gotanswer = True
+            return True
+        elif x.lower() == "n" or x.upper() == "NO":
+            gotanswer = True
+            return False      
 
 # Ask question and define result
-if get_answer(q_txt):
-    print('YES')
+if get_answer(data['Q']):
+    print('Yes')
 else:
     print('No')
